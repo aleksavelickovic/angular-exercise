0bc23db577dcb1814f051da2b97a8375fbeb07e7
aleksavelickovic <velickovic.sr44.2023@uns.ac.rs>
2025-10-15 02:38:31 +0200

    Add Home component and update routing; enhance navigation bar
    TODO: Find a better way to import styles

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -2,10 +2,12 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {Wines} from './wine/wines/wines';
 import {AddWine} from './wine/add-wine/add-wine';
+import {Home} from './layout/home/home';
 
 const routes: Routes = [
   {component: Wines, path: 'wines'},
-  {component: AddWine, path: 'add'}
+  {component: AddWine, path: 'add'},
+  {component: Home, path: 'home'}
 ];
 
 @NgModule({
